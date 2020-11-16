ba66b4cd74dd6fcf43ca39152878d753cf500073
Mostafa <mai.mostafa@peelsb.com>
2020-11-16 15:51:28 -0500

    adding the dropdowns and checkboxes dynamically from the list + implementing the update feature

--- src/webparts/mergedCalendar/components/MergedCalendar.tsx
+++ src/webparts/mergedCalendar/components/MergedCalendar.tsx
@@ -10,32 +10,22 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import rrulePlugin from '@fullcalendar/rrule';
 
-import {Panel, DefaultButton, PrimaryButton, Checkbox, Stack, Dropdown, IDropdownStyles, IDropdownOption} from '@fluentui/react';
+import {Panel, DefaultButton, PrimaryButton, Checkbox, Stack, Dropdown, IDropdownOption, Spinner} from '@fluentui/react';
 import {useBoolean} from '@fluentui/react-hooks';
 
 import {CalendarOperations} from '../Services/CalendarOperations';
-import {getCalSettings} from '../Services/CalendarRequests';
+import {getCalSettings, updateCalSettings} from '../Services/CalendarSettingsOps';
 
 export default function MergedCalendar (props:IMergedCalendarProps) {
   
   const _calendarOps = new CalendarOperations();
-  const [eventSources, setEventSources] = React.useState(props.eventSources);
+  const [eventSources, setEventSources] = React.useState([]);
   const [calSettings, setCalSettings] = React.useState([]);
   const [isOpen, { setTrue: openPanel, setFalse: dismissPanel }] = useBoolean(false);
-  const stackTokens = { childrenGap: 10 };
-  const options: IDropdownOption[] = [
-    { key: 'E1Day', text: 'Day 1 Cycle' },
-    { key: 'E2Day', text: 'Day 2 Cycle' },
-    { key: 'E3Day', text: 'Day 3 Cycle' },
-    { key: 'E4Day', text: 'Day 4 Cycle' },
-    { key: 'E5Day', text: 'Day 5 Cycle' },
-    { key: 'E6Day', text: 'Day 6 Cycle' },
-    { key: 'E7Day', text: 'Day 7 Cycle' },
-    { key: 'E8Day', text: 'Day 8 Cycle' },
-    { key: 'E9Day', text: 'Day 9 Cycle' },
-    { key: 'E10Day', text: 'Day 10 Cycle' },
-  ];
+  const [isChecked, setIsChecked] = React.useState(true);
 
+  const stackTokens = { childrenGap: 20 , maxWidth: 250};
+  
   function handleDateClick(arg:any){
     alert(arg.dateStr);
   }
@@ -47,7 +37,26 @@ export default function MergedCalendar (props:IMergedCalendarProps) {
     getCalSettings(props.context, props.calSettingsList).then((result:{}[])=>{
       setCalSettings(result);
     })
-  })
+  },[eventSources.length])
+
+  const chkHandleChange = (calSettings:{})=>{
+    return (ev: any, checked: boolean) => { 
+      updateCalSettings(props.context, props.calSettingsList, calSettings, checked).then(()=>{
+        _calendarOps.displayCalendars(props.context, props.calSettingsList).then((result:{}[])=>{
+          setEventSources(result);
+        });
+      })
+     }
+  }
+  const dpdHandleChange = (calSettings:any)=>{
+    return (ev: any, item: IDropdownOption) => { 
+      updateCalSettings(props.context, props.calSettingsList, calSettings, calSettings.ShowCal, item.key).then(()=>{
+        _calendarOps.displayCalendars(props.context, props.calSettingsList).then((result:{}[])=>{
+          setEventSources(result);
+        });
+      })
+     }
+  }
 
   const onRenderFooterContent = React.useCallback(
     () => (
@@ -70,22 +79,24 @@ export default function MergedCalendar (props:IMergedCalendarProps) {
         onDismiss={dismissPanel}
         headerText="Calendar Settings"
         closeButtonAriaLabel="Close"
-        onRenderFooterContent={onRenderFooterContent}
+        //onRenderFooterContent={onRenderFooterContent}
         isFooterAtBottom={true}
-      >
+        className={styles.calendarPanel}>
+
         <Stack tokens={stackTokens}>
-          <Checkbox label="My School" disabled defaultChecked />
-          <Checkbox label="Rotary" />
-          <Dropdown
-            placeholder="Select an option"            
-            options={options}
-          />    
-          {/* {calSettings.map((data, id)=>{
-            <Checkbox key={id} label={data.Title} />
-          })}       */}
-          <Checkbox label="Test 2" />
-          <Checkbox label="Test 3" />
+          {calSettings.map((value, index) => {        
+            return (
+              <div>
+                <Checkbox key={index} onChange={chkHandleChange(value)} defaultChecked={value.Chkd} label={value.Title} disabled={value.Disabled} />
+                {value.Dpd &&
+                  <Dropdown onChange={dpdHandleChange(value)} className={styles.marginT5} placeholder="Select Day..."  defaultSelectedKey={value.CalName} options={props.dpdOptions} />
+                }
+              </div>
+            )
+          })}
         </Stack>
+
+        {/* <Spinner label="Please Wait, Calendars are updating..." ariaLive="assertive" labelPosition="right" /> */}
       </Panel>
 
       <div className={styles.calendarCntnr}>
@@ -107,7 +118,7 @@ export default function MergedCalendar (props:IMergedCalendarProps) {
           dateClick={handleDateClick}
           eventSources = {eventSources}
         />
-      </div>
+      </div> 
 
     </div>
   );
