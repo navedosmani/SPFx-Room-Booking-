04b411561bc42ff3baefb6b3146c4289406e6c85
Maya-Mostafa <mayamostafa@gmail.com>
2020-11-30 14:02:12 -0500

    adding a header to the panel

--- src/webparts/mergedCalendar/components/ICalSettings/ICalSettings.tsx
+++ src/webparts/mergedCalendar/components/ICalSettings/ICalSettings.tsx
@@ -2,26 +2,34 @@ import * as React from 'react';
 import styles from '../MergedCalendar.module.scss';
 import { ICalSettingsProps } from './ICalSettingsProps';
 
-import {Checkbox, Stack, Dropdown} from '@fluentui/react';
+import {Checkbox, Stack, Dropdown, Label, Text} from '@fluentui/react';
+import { initializeIcons } from '@uifabric/icons';
+import { Icon } from '@fluentui/react/lib/Icon';
 
 export default function ICalSettings (props:ICalSettingsProps) {
 
     const stackTokens = { childrenGap: 20 , maxWidth: 250};    
+    initializeIcons();
+    const CalendarIcon = () => <Icon iconName="Calendar" />;
 
     return(
         <div className={styles.calendarPanel}>
+            <h3 className={styles.panelHeader}><Label><CalendarIcon />Choose Calendar(s)</Label></h3>
             <Stack tokens={stackTokens}>
                 {props.calSettings.map((value:any) => {        
                     return (
-                        <div>
-                        <Checkbox key={value.Id} onChange={props.onChkChange(value)} defaultChecked={value.Chkd} label={value.Title} disabled={value.Disabled} />
-                        {value.Dpd &&
-                            <Dropdown onChange={props.onDpdChange(value)} className={styles.marginT5} placeholder="Select Day..."  defaultSelectedKey={value.CalName} options={props.dpdOptions} />
-                        }
+                        <div>                            
+                            <Checkbox key={value.Id} onChange={props.onChkChange(value)} defaultChecked={value.Chkd} label={value.Title} disabled={value.Disabled} />
+                            {value.Dpd &&
+                                <Dropdown onChange={props.onDpdChange(value)} className={styles.marginT5} placeholder="Select Day..."  defaultSelectedKey={value.CalName} options={props.dpdOptions} />
+                            }
                         </div>
                     );
                 })}
             </Stack>
+            {/* <hr className={styles.panelSeparator}/>
+            <h3 className={styles.panelHeader}><Label><CalendarSettingsIcon />Weekends</Label></h3>
+            <Checkbox label="Show Weekends" defaultChecked={props.showWeekends} /> */}
         </div>
     );
 }
