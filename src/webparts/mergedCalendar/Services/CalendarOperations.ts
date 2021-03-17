4259a8e6a56ca0d25a45f0c38328ea7df922f541
Maya-Mostafa <mayamostafa@gmail.com>
2021-03-17 19:24:12 -0400

    returning calsettings and events in one call instead of calling it twice in App

--- src/webparts/mergedCalendar/Services/CalendarOperations.ts
+++ src/webparts/mergedCalendar/Services/CalendarOperations.ts
@@ -6,19 +6,21 @@ import {getCalSettings} from '../Services/CalendarSettingsOps';
 export class CalendarOperations{
     
 
-    public displayCalendars(context: WebPartContext , calSettingsListName:string): Promise <{}[]>{
+    public displayCalendars(context: WebPartContext , calSettingsListName:string, roomId?: number): Promise <{}[]>{
         
         console.log("Display Calendar Function");
 
         let eventSources : {}[] = [], 
-            eventSrc  : {} = {};
+            eventSrc  : {} = {},
+            calSettings : any = [];
 
         // `async` is needed since we're using `await`
         return getCalSettings(context, calSettingsListName).then(async (settings:any) => {
+            calSettings = settings;
             const dataFetches = settings.map(setting => {
                 // This `return` is needed otherwise `undefined` is returned in this `map()` call.
                 if(setting.ShowCal){
-                    return getCalsData(context, setting).then((events: any) => {
+                    return getCalsData(context, setting, roomId).then((events: any) => {
                         eventSrc = {
                             events: events,
                             color: setting.BgColorHex,
@@ -32,7 +34,7 @@ export class CalendarOperations{
             
             // The next then takes the eventSources array and it becomes the return value.
             // Its a one-liner so `return` is implicitly known here
-        }).then(() => eventSources);
+        }).then(() => [calSettings, eventSources]);
     }
 
    
