13a9db52e51bef5e1f76642e43c8cf03de15f7e4
Maya-Mostafa <mayamostafa@gmail.com>
2021-03-18 11:10:11 -0400

    writing the location goup function

--- src/webparts/mergedCalendar/Services/RoomOperations.ts
+++ src/webparts/mergedCalendar/Services/RoomOperations.ts
@@ -8,3 +8,25 @@ export const getRooms = async (context: WebPartContext, roomsList: string) =>{
 
     return results.value;
 };
+
+
+export const getLocationGroup = async(context: WebPartContext, roomsList: string) =>{
+    console.log("Get Rooms Location Group Function");
+    const restUrl = context.pageContext.web.absoluteUrl + `/_api/web/lists/GetByTitle('${roomsList}')/fields?$filter=EntityPropertyName eq 'LocationGroup'`;
+    const results = await context.spHttpClient.get(restUrl, SPHttpClient.configurations.v1).then(response => response.json());
+
+    return adjustLocation(results.value[0].Choices);
+};
+const adjustLocation = (arr: []): {}[] =>{
+    let arrAdj :{}[] = [];
+    arrAdj.push({key: 'all', text:'All'});
+
+    arr.map((item: string)=>{
+        arrAdj.push({
+            key: item.toLowerCase(),
+            text: item
+        })
+    });
+
+    return arrAdj;
+};
